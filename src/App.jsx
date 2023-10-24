eb630150f4831da6e48a683f93a5005d147bf23c
Santiago Ortega <dsantiagortega@hotmail.com>
2023-10-23 19:42:02 -0500

    formCow

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login";
 import { FormUserRegister } from "./components/FormUserRegister";
 import { MainPage } from "./components/MainPage";
+import { FormCow } from "./components/FormCow";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage/>} />
+        <Route path="/" element={<FormCow/>} />
         <Route path="/form-example" element={<FormUserRegister />} />
         <Route path="/main-page" element={<MainPage />} />
       </Routes>
