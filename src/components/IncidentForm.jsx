1a53683a3c82cec7e74b3924019b0f6abde776b9
Nicolas Gil <53237431+Neecs@users.noreply.github.com>
2023-10-24 10:52:46 -0500

    data from inc form

--- src/components/IncidentForm.jsx
+++ src/components/IncidentForm.jsx
@@ -5,6 +5,22 @@ import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 
 export const IncidentForm = () => {
+  const handleSumbit = (e) => {
+    const form = e.currentTarget;
+    e.preventDefault();
+
+    const name = form.name.value;
+    const dateIn = form.dateIn.value;
+    const description = form.description.value;
+
+    const data = {
+      name,
+      dateIn,
+      description,
+    };
+
+    console.log(data);
+  };
 
   return (
     <div className="inc-form-page">
@@ -12,7 +28,7 @@ export const IncidentForm = () => {
       <br />
       <br />
       <div className="inc-form">
-        <Form>
+        <Form onSubmit={handleSumbit}>
           <div className="inc-name">
             <Form.Group as={Col} md="4" controlId="name">
               <Form.Label>Nombre</Form.Label>
@@ -20,16 +36,13 @@ export const IncidentForm = () => {
             </Form.Group>
           </div>
           <div className="date-in">
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3" controlId="dateIn">
               <Form.Label>Fecha</Form.Label>
               <Form.Control type="date" placeholder="name@example.com" />
             </Form.Group>
           </div>
           <div className="text-area-des">
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
+            <Form.Group className="mb-3" controlId="description">
               <Form.Label>Descripción</Form.Label>
               <Form.Control as="textarea" rows={3} />
             </Form.Group>
