4d756f5dd769b2a198052624e94acf5a1951e461
Sebastian Palacios <sebpal005@gmail.com>
2023-11-07 09:49:19 -0500

    HV new styles

--- src/components/Cows/HVCow/FormCvCow.jsx
+++ src/components/Cows/HVCow/FormCvCow.jsx
@@ -6,178 +6,140 @@ import Row from "react-bootstrap/Row";
 import "./formCvCow.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import Alert from "react-bootstrap/Alert";
-import { updateHistory } from "../../../supabase/usecases/cows/update_cow.js";
-import {supabase} from "../../../supabase/data/constants/api_credentials.js";
+import { createCow } from "../../../supabase/usecases/cows/create_cow.js";
+import { supabase } from "../../../supabase/data/constants/api_credentials.js";
 
 export const FormCvCow = () => {
-  const [validated, setValidated] = useState(false);
-  const [errorSignUp, setErrorSignUp] = useState(false);
-  const [errorPassword, setErrorPassword] = useState(false);
-  const [razesDictionary, setRazesDictionary] = useState({});
-  const [genderDictionary, setGenderDictionary] = useState({});
-  const [selectedGender, setSelectedGender] = useState("");
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    async function fetchData() {
-      
-    }
-    fetchData();
-  }, []);
-
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    event.preventDefault();
-
-    const color = form.color.value;
-    const name = form.name.value;
-    const idHato = form.hato.value;
-    const idPerson = form.person.value;
+    const [validated] = useState(false);
     
 
-    supabase.auth.onAuthStateChange((event, session) => {
-      const createNewCow = async () => {
-        await updateHistory(color, name, idHato, idPerson,session.user.id);
-      };
-      createNewCow();
-      navigate('/')
+    const navigate = useNavigate();
 
-    });
-  }
+    useEffect(() => {
+        async function fetchData() {
+           
+        }
+        fetchData();
+    }, []);
 
-  return (
-    <div className="form-cv-cow">
-      <div className="form-space-cv">
-        <div className="header-title-cv">
-          <h4 className="title">Hoja de vida</h4>
-          <p className="category">Estos son los datos</p>
-        </div>
-        <div className="data-cow">
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Col md="3">
-              <h4>Información principal</h4>
-            </Col>
-            <Row className="mb-3">
-              <Form.Group as={Col} md="4" controlId="color">
-                <Form.Select
-                  aria-label="Cambiar el color"
-                  required
-                  onChange={(e) => (e.target.value)}
-                >
-                </Form.Select>
-                <Form.Control.Feedback type="invalid">
-                  Seleccione un color valido
-                </Form.Control.Feedback>
-              </Form.Group>
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        event.preventDefault();
 
-              <Form.Group as={Col} md="5" controlId="bornDate">
-                <Form.Floating className="mb-3">
-                  <Form.Control type="date" required />
-                  <Form.Label>Fecha de nacimiento del bovino</Form.Label>
-                  <Form.Control.Feedback type="invalid">
-                    Introduce la fecha de nacimiento.
-                  </Form.Control.Feedback>
-                </Form.Floating>
-              </Form.Group>
-            </Row>
+        const razeId = form.breed.value;
+        const bornDate = form.bornDate.value;
+        const genderId = form.genre.value;
+        const name = form.name.value;
 
-            <Row className="mb-3">
-              <Form.Group as={Col} md="4" controlId="genre">
-                <Form.Select
-                  aria-label="Default select example"
-                  required
-                  value={selectedGender}
-                  onChange={(e) => setSelectedGender(e.target.value)}
-                >
-                  <option value="">Genero</option>
-                  {Object.keys(genderDictionary).map((key) => (
-                    <option key={key} value={key}>
-                      {genderDictionary[key]}
-                    </option>
-                  ))}
-                </Form.Select>
-                <Form.Control.Feedback type="invalid">
-                  Selecciona el sexo del Bovino.
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-            <Col md="3">
-              <h9>Información adicional</h9>
-            </Col>
-            <Row className="mb-3">
-              <Form.Group as={Col} md="4" controlId="name">
-                <Form.Floating className="mb-3">
-                  <Form.Control type="text" placeholder=" " required />
-                  <Form.Label>Nombre del bovino</Form.Label>
-                  <Form.Control.Feedback type="invalid">
-                    Introduce un nombre.
-                  </Form.Control.Feedback>
-                </Form.Floating>
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="color">
-                <Form.Floating className="mb-3">
-                  <Form.Control type="text" placeholder=" " required />
-                  <Form.Label>Color del bovino</Form.Label>
-                  <Form.Control.Feedback type="invalid">
-                    Ingrese un color.
-                  </Form.Control.Feedback>
-                </Form.Floating>
-              </Form.Group>
-            </Row>
-            <Col md="3">
-              <h9>Información del hato</h9>
-            </Col>
-            <Row className="mb-3">
-              <Form.Group as={Col} md="4" controlId="nameHato">
-                <Form.Floating className="mb-3">
-                  <Form.Control type="text" placeholder=" " required />
-                  <Form.Label>Nombre del Hato</Form.Label>
-                  <Form.Control.Feedback type="invalid">
-                    Introduce un nombre.
-                  </Form.Control.Feedback>
-                </Form.Floating>
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="location">
-                <Form.Floating className="mb-3">
-                  <Form.Control type="text" placeholder=" " required />
-                  <Form.Label>Ubicacion del Hato</Form.Label>
-                  <Form.Control.Feedback type="invalid">
-                    Ingrese un color.
-                  </Form.Control.Feedback>
-                </Form.Floating>
-              </Form.Group>
-            </Row>
+        supabase.auth.onAuthStateChange((event, session) => {
+            const createNewCow = async () => {
+                await createCow(
+                    razeId,
+                    genderId,
+                    bornDate,
+                    name,
+                    session.user.id
+                );
+            };
+            createNewCow();
+            navigate("/");
+        });
+    };
 
-            <div className="buttons-cow">
-              <Form.Group className="mb-3"></Form.Group>
-              <Button type="submit" className="btn btn-dark btn-lg">
-                Registrar vaca
-              </Button>
-              <span style={{ marginRight: "10px" }}></span>
-              <Link to="/">
-                <Button type="button" className="btn btn-dark btn-lg">
-                  Regresar
-                </Button>
-              </Link>
+    return (
+        <div className="form-cow-cv">
+            <div className="form-space-cow">
+                <div className="header-title-cow">
+                    <h4 className="title">Hoja de vida</h4>
+                    {/* <p className="category">Ingrese los datos del Bovino</p> */}
+                </div>
+                <div className="data-cow">
+                    <Form
+                        noValidate
+                        validated={validated}
+                        onSubmit={handleSubmit}
+                    >
+                        <Col md="3">
+                            <h2 className="subtitle">Información</h2>
+                        </Col>
+                        <Row className="mb-3">
+                            <Form.Group as={Col} md="3" controlId="genre">
+                                <Form.Floating className="mb-3">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder=" "
+                                        required
+                                    />
+                                    <Form.Label>Color</Form.Label>
+                                    <Form.Control.Feedback type="invalid">
+                                        Cambia el Color.
+                                    </Form.Control.Feedback>
+                                </Form.Floating>
+                            </Form.Group>
+                            <Form.Group as={Col} md="3" controlId="genre">
+                                <Form.Floating className="mb-3">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder=" "
+                                        required
+                                    />
+                                    <Form.Label>Nombre</Form.Label>
+                                    <Form.Control.Feedback type="invalid">
+                                        Cambia el nombre.
+                                    </Form.Control.Feedback>
+                                </Form.Floating>
+                            </Form.Group>
+                        </Row>
+
+                        <Col md="3">
+                            <h2 className="subtitle">Ubicación y dueño</h2>
+                        </Col>
+                        <Row className="mb-3">
+                            <Form.Group as={Col} md="3" controlId="genre">
+                                <Form.Floating className="mb-3">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder=" "
+                                        required
+                                    />
+                                    <Form.Label>Hato</Form.Label>
+                                </Form.Floating>
+                            </Form.Group>
+                            <Form.Group as={Col} md="3" controlId="genre">
+                                <Form.Floating className="mb-3">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder=" "
+                                        required
+                                    />
+                                    <Form.Label>Nombre Dueño</Form.Label>
+                                    <Form.Control.Feedback type="invalid">
+                                        Cambia el nombre.
+                                    </Form.Control.Feedback>
+                                </Form.Floating>
+                            </Form.Group>
+                        </Row>
+                        <div className="buttons-cow">
+                            <Form.Group className="mb-3"></Form.Group>
+                            <Button
+                                type="submit"
+                                className="button-common button-aceptar"
+                            >
+                                Modificar
+                            </Button>
+                            <span style={{ marginRight: "10px" }}></span>
+                            <Link to="/">
+                                <Button
+                                    type="button"
+                                    className="button-common button-regresar"
+                                >
+                                    Regresar
+                                </Button>
+                            </Link>
+                        </div>
+                    </Form>
+                </div>
             </div>
-
-            <br />
-            <br />
-            {errorSignUp && (
-              <Alert key="danger" variant="danger">
-                Ya existe un usuario con este correo
-              </Alert>
-            )}
-            {errorPassword && (
-              <Alert key="danger" variant="danger">
-                Las contraseñas no coinciden
-              </Alert>
-            )}
-          </Form>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
